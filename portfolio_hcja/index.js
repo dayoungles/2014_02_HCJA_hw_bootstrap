f09b6988a6adfe60bbb3d994c11302904855f9df
dayoungles <dayoungles@naver.com>
2014-08-23 21:57:32 +0900

    좌우로 움직인다. 다음에는 li 를 떼서 뒤로 옮기는 작업을 한다.

--- portfolio_hcja/index.js
+++ portfolio_hcja/index.js
@@ -2,38 +2,64 @@
 var count=0;
 
 var moveRight = function() {
-	var chain = document.getElementsByClassName("chain");
-	var len = chain.length;
-	var move, style;
+
+	var elBackboards = document.getElementsByClassName("list");
+	var move;
+	var len = elBackboards.length;
 	
-	for(var i = 0; i< len; i++){
-		var item = chain[i];
-		style = window.getComputedStyle(item);
-		if(i===1){
-			move = 10;			
-		} else {
-			move = -10;
-		}
-		var leftValue = parseInt(style.left);
-		item.style.left = (leftValue+ move) +'px';
+	for(var i = 0; i<len; i++){
+		var item = elBackboards[i];
+		//1,2,3번에 따라서 좌, 우로 이동방향과 속도를 결정해야한다.
+		var name = item.className;
 		
-		var afterMove = window.getComputedStyle(item);
-		var afterMoveLeft = afterMove.left;
-		console.log(afterMoveLeft);
-		if(afterMoveLeft === "0%");
+		console.log(name);
+		if(name.indexOf("left") != -1){
+			if(name.indexOf("slow") != -1) {
+				move = -5;
+			} else {
+				move = -8;
+			}
+		} else {
+			move = 5;
+		} 
+		
+		for(var j = 0; j < item.children.length; j++){
+			var style = window.getComputedStyle(item.children[j]);
+			var leftValue = style.left;
+			console.log(leftValue);
+			leftValue = parseInt(leftValue) + move;
+			item.children[j].style.left = leftValue+"px";
+		}
+
 	}
-	
+
 
 };
 var timer = setInterval(function() {
 		moveRight();
 		count++;
-		if(count===100)	clearInterval(timer);
+		if(count===1000)	clearInterval(timer);
+		
 	},100 );
 	
 	
-	
-
+var split = function() {
+	//화면 width를 구한다. 지금은 퍼센테이지로 설정되어 있어서, 언제 left값이 -45%가 되는지 확인할 수 없다.
+	//li첫번째 요소가 -45%까지 가면, 떼서 옮긴다. 
+	var width = screen.width;
+	var liBackboards = document.getElementsByClassName("backboard");
+	for(var i = 0; i < liBackboards.length; i++){
+		var elBackboard = liBackboards[i];
+		var style = window.getComputedStyle(elBackboard);
+		var left = style.left;
+/*
+		if(left <){
+			//여기에서 떼서 붙인다. 
+		}
+*/
+	}
+	 
+}
 
 
 
