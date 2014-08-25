7307d093ad1e74fcb0db2feed51b1db05c5926c2
dayoungles <dayoungles@naver.com>
2014-08-25 12:28:50 +0900

    하드코딩한거 이제 리팩토링해서 새 파일로 옮길 예정.

--- portfolio_hcja/index.js
+++ portfolio_hcja/index.js
@@ -3,63 +3,99 @@ var count=0;
 
 var moveRight = function() {
 
-	var elBackboards = document.getElementsByClassName("list");
+	var ulLists = document.getElementsByClassName("list");
 	var move;
-	var len = elBackboards.length;
+	var len = ulLists.length;
 	
 	for(var i = 0; i<len; i++){
-		var item = elBackboards[i];
+		var item = ulLists[i];
 		//1,2,3번에 따라서 좌, 우로 이동방향과 속도를 결정해야한다.
 		var name = item.className;
 		
-		console.log(name);
+
 		if(name.indexOf("left") != -1){
 			if(name.indexOf("slow") != -1) {
 				move = -5;
 			} else {
-				move = -8;
+				move = -3;
 			}
 		} else {
-			move = 5;
+			move = 3;
 		} 
 		
 		for(var j = 0; j < item.children.length; j++){
 			var style = window.getComputedStyle(item.children[j]);
 			var leftValue = style.left;
-			console.log(leftValue);
 			leftValue = parseInt(leftValue) + move;
 			item.children[j].style.left = leftValue+"px";
 		}
-
 	}
-
-
 };
-var timer = setInterval(function() {
-		moveRight();
-		count++;
-		if(count===1000)	clearInterval(timer);
-		
-	},100 );
+
 	
+
 	
 var split = function() {
-	//화면 width를 구한다. 지금은 퍼센테이지로 설정되어 있어서, 언제 left값이 -45%가 되는지 확인할 수 없다.
-	//li첫번째 요소가 -45%까지 가면, 떼서 옮긴다. 
 	var width = screen.width;
-	var liBackboards = document.getElementsByClassName("backboard");
-	for(var i = 0; i < liBackboards.length; i++){
-		var elBackboard = liBackboards[i];
-		var style = window.getComputedStyle(elBackboard);
-		var left = style.left;
-/*
-		if(left <){
-			//여기에서 떼서 붙인다. 
+	var ulLists = document.getElementsByClassName("list");
+
+ 	for(var i = 0; i < ulLists.length; i++){ 
+		var elList = ulLists[i];
+		
+		for(var j = 0; j < elList.children.length; j++){
+			var style = window.getComputedStyle(elList.children[j]);
+			var left = style.left;
+			left = parseInt(left);						
+			if(i !=1) {
+				if(left < -width){
+					var liChildren = elList.firstElementChild;
+					
+					var clone = liChildren.cloneNode(true);
+					clone.style.left = left + 4500 + "px"; 
+					elList.appendChild(clone);
+					elList.removeChild(liChildren);
+				}
+			} else {
+				if(left > width){
+					var liChildren = elList.lastElementChild;
+					
+					var clone = liChildren.cloneNode(true);
+					clone.style.left = left - 4500 + "px"; 
+					elList.insertAdjacentElement("afterBegin", clone);
+					elList.removeChild(liChildren);
+				}
+			}
 		}
-*/
-	}
-	 
+ 	} 	 
 }
 
 
+var timer = setInterval(function() {
+		moveRight();
+		split();
+	},100 );
+	
+var event = function() {
+	var ulLists = document.getElementsByClassName("list");
+	for (var i = 0; i < ulLists.length; i++){
+		ulLists[i].addEventListener("mouseover", function() {
+			console.log("mouseover");
+			clearInterval(timer);
+		});
+	
+			
+		ulLists[i].addEventListener("mouseout", function() {
+			console.log("mouseout + addevent");
+			timer = setInterval(function() {
+					moveRight();
+					split();
+				}, 100);
+			});
+	}
+}
 
+event();	
+
+//1. 코드 리팩토링 to constructor
+//2. 셋인터벌 한줄씩 영향 받도록 ul맏각각 달기.
+//3. 
